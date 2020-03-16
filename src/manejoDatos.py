d1b02da0d2aa31c9282d72ab93c920dc33433f28
Migue <milojama@gmail.com>
2020-03-16 21:00:14 +0100

    Acabados test pytest

--- src/manejoDatos.py
+++ src/manejoDatos.py
@@ -1,6 +1,10 @@
+import os 
+dir = os.path.dirname(__file__) 
+dir2 = os.path.dirname(__file__) 
+dir3 = os.path.dirname(__file__)
+
 class ManejadorDatos:
     
-
     def __init__(self,ruta):
         self.lugares = []
         
@@ -8,11 +12,14 @@ class ManejadorDatos:
 
     def extraerDatos(self,ruta):
  
-        archivo = open(ruta,'r')
-        linea = archivo.readline()
-        lugares.append(linea)
-        print lugares
-    
+        filename3 = os.path.join(dir3, ruta)
+        archivo = open(filename3,'r')
+        for linea in archivo:
+            self.lugares.append(linea)
+            #linea = archivo.readline()
+        
+        print (self.lugares)
+        return self.lugares
 
 
     def guardarDatos(self):
@@ -22,10 +29,21 @@ class ManejadorDatos:
         pass
 
 
-ruta = '/home/angela/Escritorio/places.txt'
-#f = open(ruta, 'r')
-#
+
+
+#Main improvisado para hacer pruebas
+
+filename = os.path.join(dir, 'places.txt')
+
+filename2 = os.path.join(dir2, 'test.txt')
+
+#ruta = 'places.txt'
+f = open(filename, 'r')
+
 #print (f.read())
 
-obj1 = ManejadorDatos(ruta) 
-ManejadorDatos.extraerDatos(obj1,ruta)
+obj1 = ManejadorDatos(filename) 
+ManejadorDatos.extraerDatos(obj1,filename)
+
+obj2 = ManejadorDatos(filename2)
+obj2.extraerDatos(filename2)
---
+++ tests/test_manejoDatos.py
@@ -0,0 +1,10 @@
+import pytest
+from src import manejoDatos
+
+PROJECTNAME = "Scanner"
+ISSUEID = 1
+
+def test_extraerDatos():
+    obj1 = manejoDatos.ManejadorDatos("test.txt")
+    assert manejoDatos.ManejadorDatos.extraerDatos(obj1, "test.txt") == ['Prueba de Texto1\n', 'Prueba de Texto2\n']
+
