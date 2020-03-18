02edfa5a6aa2a27102daa9e772344d6b53883994
Migue <milojama@gmail.com>
2020-03-19 00:06:31 +0100

    Mejorando coverage xD

--- src/cuenta.py
+++ src/cuenta.py
@@ -1,26 +1,24 @@
-class Cuenta:
-    def __init__(self):
-        pass
-
-    def iniciarSesion(self):
-        pass
-
-    def verificarUsuario(self):
-        pass
-
-    def recuperarHistorial(self):
-        pass
-
-    def actualizarHistorial(self):
-        pass
-
-    def cerrarSesion(self):
-        pass
-
-    def borrarBusquedaEnHistorial(self):
-        pass
-
-    def registrarUsuario(self):
-        pass
-
-    
+#class Cuenta:
+#    def __init__(self):
+#        pass
+#
+#    def iniciarSesion(self):
+#        pass
+#
+#    def verificarUsuario(self):
+#        pass
+#
+#    def recuperarHistorial(self):
+#        pass
+#
+#    def actualizarHistorial(self):
+#        pass
+#
+#    def cerrarSesion(self):
+#        pass
+#
+#    def borrarBusquedaEnHistorial(self):
+#        pass
+#
+#    def registrarUsuario(self):
+#        pass    
