e654bb1562aa538446aac1bec84765123d7a7098
Angela <gallardoangela.ruiz@gmail.com>
2020-03-30 19:16:53 +0200

    Actualizado test_abstractBD.py

--- tests/test_abstractBD.py
+++ tests/test_abstractBD.py
@@ -4,6 +4,14 @@ from src import abstractBD
 PROJECTNAME = "Scanner"
 ISSUEID = 3
 
-def test_itialiceBD():
+def test_initialiceBD():
     obj1 = abstractBD.BaseDatos()
     assert abstractBD.BaseDatos.initialiceBD(obj1) == 555
+
+def test_getDataBD():
+    obj1 = abstractBD.BaseDatos()
+    assert abstractBD.BaseDatos.getData(obj1) == 556
+
+def test_setData():
+    obj1 = abstractBD.BaseDatos()
+    assert abstractBD.BaseDatos.setData(obj1) == 557
