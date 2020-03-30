508f80601c21f7e8d626ddeb4fb62d488b4b8615
Angela <gallardoangela.ruiz@gmail.com>
2020-03-31 00:28:17 +0200

    Tests corregidos

--- tests/test_inyectoraBD.py
+++ tests/test_inyectoraBD.py
@@ -10,8 +10,9 @@ def test_initialiceBD():
     assert inyectoraBD.Inyectora.initialiceBD(obj1) == 558
 
 def test_setData():
+    nuevo = ('migueorg', 'Migue', 'migueorg@correo.ugr.es')
     obj1 = inyectoraBD.Inyectora()
-    assert inyectoraBD.Inyectora.setData(obj1) == 559
+    assert inyectoraBD.Inyectora.setData(obj1,nuevo) == 559
 
 def test_getDataBD():
     obj1 = inyectoraBD.Inyectora()
